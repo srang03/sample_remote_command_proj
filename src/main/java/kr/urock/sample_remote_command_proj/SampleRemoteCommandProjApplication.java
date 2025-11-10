a02b890a20d0c4ad97c1b5ceac714ba529e13eba
ian <ian@urock.kr>
2025-11-10 22:02:59 +0900

    [init] proj init

---
+++ src/main/java/kr/urock/sample_remote_command_proj/SampleRemoteCommandProjApplication.java
@@ -0,0 +1,13 @@
+package kr.urock.sample_remote_command_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRemoteCommandProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleRemoteCommandProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/urock/sample_remote_command_proj/SampleRemoteCommandProjApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.urock.sample_remote_command_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRemoteCommandProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
